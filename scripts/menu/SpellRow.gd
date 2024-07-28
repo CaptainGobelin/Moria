46c030c0a1d539e03f70d6b68849932cc108756f
CaptainGobelin <captaingobelin@gmail.com>
2024-07-28 21:18:32 +0200

    Add choose spell menu

--- scripts/menu/SpellRow.gd
+++ scripts/menu/SpellRow.gd
@@ -1,21 +1,34 @@
 extends Node2D
 
 onready var selected = get_node("Selected")
+onready var extended = get_node("Selected/Extended")
 onready var icon = get_node("Icon")
 onready var shortcut = get_node("TextContainer/Shortcut")
 onready var spellName = get_node("TextContainer/Name")
 onready var remaining = get_node("TextContainer/Remaining")
 onready var uses = get_node("TextContainer/Uses")
 
-var spell
+var spell: int
+
+func setSimpleContent(spellId: int, spellIcon: int, spellName: String, rank: int):
+	spell = spellId
+	self.spellName.text = spellName
+	icon.frame = spellIcon
+	remaining.text = Utils.toRoman(rank)
+	remaining.align = Label.ALIGN_CENTER
+	uses.text = ""
+	shortcut.text = ""
+	extended.visible = false
 
 func setContent(spellRow):
 	spell = spellRow[0]
 	spellName.text = spellRow[1]
 	icon.frame = spellRow[2]
 	remaining.text = String(spellRow[3])
+	remaining.align = Label.ALIGN_RIGHT
 	uses.text = "/" + String(spellRow[4])
 	if spellRow[5] != null:
 		shortcut.text = String(spellRow[5])
 	else:
 		shortcut.text = ""
+	extended.visible = true
