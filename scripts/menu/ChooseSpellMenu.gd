80dd5dfca51bbb15524001409241ec81343a1ae8
CaptainGobelin <captaingobelin@gmail.com>
2024-08-08 21:36:20 +0200

    Fix scrolling in choose spell menu

--- scripts/menu/ChooseSpellMenu.gd
+++ scripts/menu/ChooseSpellMenu.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
+onready var scroller = get_node("MenuScroller")
 onready var descriptor = get_node("SpellDescription")
 onready var rows = get_node("SpellList")
 
 var selected: int = 0
 var currentList: Array = []
 var source = null
+var startRow = 0
 
 func _ready():
 	set_process_input(false)
 
 func open(school: int, level: int, caller):
 	selected = 0
+	startRow = 0
 	source = caller
 	visible = true
 	Ref.currentLevel.visible = false
@@ -30,7 +33,7 @@ func open(school: int, level: int, caller):
 				currentList.append(s)
 	if currentList.empty():
 		close()
-	loadSpellList(rank)
+	loadSpellList()
 
 func close():
 	visible = false
@@ -40,34 +43,39 @@ func close():
 	if source != null:
 		source.open()
 
-func loadSpellList(rank: int):
-	var count = 0
-	for s in currentList:
-		if count > 6:
-			return
-		rows.get_child(count).setSimpleContent(s)
-		count += 1
-	for i in range(count, 6):
-		rows.get_child(i).visible = false
-	refreshSelection()
-
-func refreshSelection():
-	var count = 0
+func loadSpellList():
+	var count = startRow
 	for row in rows.get_children():
+		if count > (currentList.size() - 1):
+			row.visible = false
+			continue
+		row.setSimpleContent(currentList[count])
+		row.visible = true
 		if count == selected:
 			row.selected.visible = true
 			selectSpell(row.spell)
 		else:
 			row.selected.visible = false
 		count += 1
+	scroller.setArrows(startRow, currentList.size())
 
 func _input(event):
 	if event.is_action_released("ui_down"):
-		selected = Utils.modulo(selected+1, currentList.size())
-		refreshSelection()
+		selected = selected + 1
+		if selected > startRow + 5:
+			startRow += 1
+		if selected >= currentList.size():
+			selected = 0
+			startRow = 0
+		loadSpellList()
 	elif event.is_action_released("ui_up"):
-		selected = Utils.modulo(selected-1, currentList.size())
-		refreshSelection()
+		selected = selected - 1
+		if selected < startRow:
+			startRow -= 1
+		if selected < 0:
+			selected = currentList.size() - 1
+			startRow = max(0, currentList.size() - 6)
+		loadSpellList()
 	elif event.is_action_released("ui_accept"):
 		var spell = rows.get_child(selected).spell
 		Ref.character.spells.learnSpell(spell)
